3569f1e03d8c8bd26223b5f22e9d98b302c5b46d
toniwidodo123 <litoninot12345@gmail.com>
2021-09-18 04:36:14 -0700

    add xss attack

--- learning-xss.php
+++ learning-xss.php
@@ -2,7 +2,8 @@
 // allow a
 // delete javascript: and onclick= dari tag
 
-echo str_replace('javascript:','',str_replace("onclick=", "",strip_tags($_GET['name'],'<a>'))) ?? 'testing';
+// echo str_replace('javascript:','',str_replace("onclick=", "",strip_tags($_GET['name'],'<a>'))) ?? 'testing';
+echo $_GET['name'];
 ?>
 
 <form method="get">
